9fac1c454516d3a26ba1f8703be3f0322cd872c9
Nicole Calamia <nikki@Nicoles-MacBook-Pro-4.local>
2022-04-27 14:09:07 -0400

    finished hover/show description for photos on bio page

--- bio-page/bio-app.js
+++ bio-page/bio-app.js
@@ -1,3 +1,27 @@
-$( () => {
+
+const hideandshow = () => {
+		const $hideandshow = $('.hide-and-show')
+		$hideandshow.toggle()
+}
+
+$(() => {
+
+const $family = $('.family')
+
+$family.hover(hideandshow)
+
+const $travel = $('.travel')
+
+$travel.hover(hideandshow)
+
+const $lyla = $('.lyla')
+
+$lyla.hover(hideandshow)
+
+const $music = $('.music')
+
+$music.hover(hideandshow)
+
+
 
 })

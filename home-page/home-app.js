e3c3699ec2a429d90eb92c8989029ba41860639b
Nicole Calamia <nikki@Nicoles-MacBook-Pro-4.local>
2022-04-27 13:34:08 -0400

    fixed dropdown for links

--- home-page/home-app.js
+++ home-page/home-app.js
@@ -1,19 +1,6 @@
 
-// Function that toggles the hidden links and makes them appear at a speed of 1000ms when hovering over "Links"
-const openLinks = () => {
-	const $hideandshow = $('.hide-and-show')
-	$hideandshow.toggle(1000)
-}
-
 
 $( () => {
-
-const $dropDownFromLinks = $('.dropDownFromLinks')
-
-// When hovering over "Links" call function "openLinks"
-$dropDownFromLinks.hover(openLinks)
-
-// .mouseleave( closeLinks);
 
 
 

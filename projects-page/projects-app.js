0d7c188d89163a2d237dcdad6a1231033cb92819
Nicole Calamia <nikki@Nicoles-MacBook-Pro-4.local>
2022-04-27 10:49:08 -0400

    getting toggle switch to work

--- projects-page/projects-app.js
+++ projects-page/projects-app.js
@@ -1,65 +1,14 @@
-//
-// // $('#switch').click(toggleSwitch)
-// //
-// // $(selector).toggle(speed,easing,callback)
-//
-// const switchOn = () => {
-// 	const $screenOneHide = $('#screen-1')
-// 	const $screenTwoShow = $('#screen-2')
-//
-// 	$screenOneHide.hide()
-// 	$screenTwoShow.show()
-// 	console.log("HI");
-//
-// }
-//
-// const switchOff = () => {
-// 	const $screenOneShow = $('#screen-1')
-// 	const $screenTwoHide = $('#screen-2')
-//
-// 	$screenOneShow.show()
-// 	$screenTwoHide.hide()
-// 	console.log("HELLO");
-// }
-//
-//
-//
-//
-//
-// $( () => {
-// $('#screen-2').hide()
-//
-// const $switch = $('#switch')
-//
-// $('body').ready() {
-// 	$switch.toggle(2000)
-// 		switchOn,
-//     switchOff,
-// 	)
-// }
-//
-//
-//
-//
-//
-// })
 
-//Use changeClass to toggle between hiding and showing screen 1 and screen 2. Need to use display: block; and display: none;
-
-// This example currently changes the background...Not working 
-
-const changeClass = () => {
-	$('body').toggleClass('on')
+// Toggles between showing and hiding screen 1 and screen 2
+const changeScreen = () => {
+	$('#screen-1').toggle()
+	$('#screen-2').toggle()
 }
 
 
 $(() => {
-	$('body').css('background-color','purple')
-	// $('body').addClass('off')
 
-
-	const $switch = $('#switch')
-	$switch.on('click', changeClass)
-
+// Calls screen toggle function when button is clicked
+$('#changeScreen').click(changeScreen)
 
 })

e5e3d082d26b45412788569cc7d9fb624d9f5ffe
Nicole Calamia <nikki@Nicoles-MacBook-Pro-4.local>
2022-04-28 19:42:04 -0400

    finished css for bio-page, home-page, and half of resume-page

--- bio-page/bio-app.js
+++ bio-page/bio-app.js
@@ -1,27 +1,39 @@
 
-const hideandshow = () => {
-		const $hideandshow = $('.hide-and-show')
-		$hideandshow.toggle()
+
+
+const hideandshowfam = () => {
+		const $hideandshowfam = $('.hide-and-show-fam')
+		$hideandshowfam.toggle('slow')
+}
+const hideandshowtrav = () => {
+		const $hideandshowtrav = $('.hide-and-show-trav')
+		$hideandshowtrav.toggle('slow')
+}
+const hideandshowpet = () => {
+		const $hideandshowpet = $('.hide-and-show-pet')
+		$hideandshowpet.toggle('slow')
+}
+const hideandshowmus = () => {
+		const $hideandshowmus = $('.hide-and-show-mus')
+		$hideandshowmus.toggle('slow')
 }
 
 $(() => {
 
 const $family = $('.family')
 
-$family.hover(hideandshow)
+$family.hover(hideandshowfam)
 
 const $travel = $('.travel')
 
-$travel.hover(hideandshow)
+$travel.hover(hideandshowtrav)
 
-const $lyla = $('.lyla')
+const $pet = $('.pet')
 
-$lyla.hover(hideandshow)
+$pet.hover(hideandshowpet)
 
 const $music = $('.music')
 
-$music.hover(hideandshow)
-
-
+$music.hover(hideandshowmus)
 
 })

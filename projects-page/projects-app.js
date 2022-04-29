e806443bea163c233197a0d92bb7cbd547e6f756
Nicole Calamia <nikki@Nicoles-MacBook-Pro-4.local>
2022-04-29 18:39:03 -0400

    css for bio-page home-page resume-page and project-page is finished

--- projects-page/projects-app.js
+++ projects-page/projects-app.js
@@ -1,4 +1,10 @@
 
+const lightbulb = () => {
+	$('#light-bulb').toggle()
+	$('#light-bulb-placeholder').toggle()
+}
+
+
 // Toggles between showing and hiding screen 1 and screen 2
 const changeScreen = () => {
 	$('#screen-1').toggle()
@@ -7,6 +13,8 @@ const changeScreen = () => {
 
 
 $(() => {
+
+$('#changeScreen').hover(lightbulb)
 
 // Calls screen toggle function when button is clicked
 $('#changeScreen').click(changeScreen)
